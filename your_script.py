e83f9dea9da3ecc85e5b3d5e073fba85de7bda43
jmihmops <alex.darenko3@yandex.ru>
2025-03-19 14:24:47 +0300

    shit

--- your_script.py
+++
@@ -1,9 +0,0 @@
-import time
-from datetime import datetime
-
-if __name__ == "__main__":
-    while True:
-        # Выводим текущее время
-        print(f"Current time: {datetime.now()}")
-        # Задержка 10 секунд для тестирования
-        time.sleep(10)
